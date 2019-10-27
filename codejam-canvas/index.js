9a1361a588067132ccbc9e9a5c4450c25ce571ba
artas0000 <anisovich28@mail.ru>
2019-10-27 22:00:28 +0300

    add dynamic canvas size

--- codejam-canvas/index.js
+++ codejam-canvas/index.js
@@ -2,19 +2,26 @@ const canvas = document.querySelector(".canvas");
 const size_4x4 = document.querySelector(".size_4x4");
 const size_32x32 = document.querySelector(".size_32x32");
 const png = document.querySelector(".png");
+const range = document.querySelector('#range');
+const span = document.querySelectorAll('span');
 
 const ctx = canvas.getContext('2d');
 canvas.height = 512;
 canvas.width = 512;
 
-function drawPnd() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const img = new Image();
-    img.src = './data/image.png';
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, canvas.height, canvas.width);
-    }
+let typeMatrix = '';
+
+function changeSize() {
+    canvas.height = range.value;
+    canvas.width = range.value;
+    canvas.style.width = `${range.value}px`;
+    canvas.style.height = `${range.value}px`;
+    span.forEach(e => {
+        e.innerHTML = range.value;
+    });
+    drawMatrix(typeMatrix);
 }
+
 
 function readTextFile(file, callback) {
     const rawFile = new XMLHttpRequest();
@@ -31,29 +38,43 @@ function readTextFile(file, callback) {
 function drawMatrix(size) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    readTextFile("./data/matrix.json", function (text) {
-        const arrays = JSON.parse(text);
-        let data = size === 4 ? arrays[0] : arrays[1];
-        let heightPixel = canvas.height / data.length;
-        let widthPixel = canvas.width / data[0].length;
-        let y = 0;
-        let x = 0;
-        data.forEach(element => {
-            element.forEach(e => {
-                ctx.fillStyle = size === 4 ? `#${e}` : `rgba(${e[0]}, ${e[1]}, ${e[2]}, ${e[3] / 255})`;
-                ctx.fillRect(x, y, widthPixel, heightPixel);
-                x += widthPixel;
+    if (size === 'png') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const img = new Image();
+        img.src = './data/image.png';
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0, canvas.height, canvas.width);
+        }
+    } else {
+        readTextFile("./data/matrix.json", function (text) {
+            const arrays = JSON.parse(text);
+            let data = size === '4x4' ? arrays[0] : arrays[1];
+            let heightPixel = canvas.height / data.length;
+            let widthPixel = canvas.width / data[0].length;
+            let y = 0;
+            let x = 0;
+            data.forEach(element => {
+                element.forEach(e => {
+                    ctx.fillStyle = size === '4x4' ? `#${e}` : `rgba(${e[0]}, ${e[1]}, ${e[2]}, ${e[3] / 255})`;
+                    ctx.fillRect(x, y, widthPixel, heightPixel);
+                    x += widthPixel;
+                });
+                x = 0;
+                y += heightPixel;
             });
-            x = 0;
-            y += heightPixel;
         });
-    });
+    }
 }
 
 size_32x32.addEventListener('click', function () {
-    drawMatrix(32)
+    typeMatrix = '32x32';
+    drawMatrix(typeMatrix)
 });
 size_4x4.addEventListener('click', function () {
-    drawMatrix(4)
+    typeMatrix = '4x4';
+    drawMatrix(typeMatrix)
 });
-png.addEventListener('click', drawPnd);
+png.addEventListener('click', function () {
+    typeMatrix = 'png';
+    drawMatrix(typeMatrix)
+});
