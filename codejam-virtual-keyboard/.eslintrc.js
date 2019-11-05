58d432ce3bbc61133dc0e6f63ac212948ea29eb8
artas0000 <anisovich28@mail.ru>
2019-11-06 00:09:18 +0300

    add click animation

--- codejam-virtual-keyboard/.eslintrc.js
+++ codejam-virtual-keyboard/.eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
--- codejam-virtual-keyboard/src/scripts/create.js
+++ codejam-virtual-keyboard/src/scripts/create.js
@@ -12,22 +12,50 @@ const ROW1 = [
     ['Digit0', '0', ')', '0', 'Ё'],
     ['Minus', '-', '_', '-', '_'],
     ['Equal', '=', '+', '=', '+'],
-    ['Backspace', '\b', '\b', '\b', '\b']
+    ['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace']
+]
+const ROW2 = [
+    ['Tab', 'Tab', 'Tab', 'Tab', 'Tab'],
+    ['KeyQ', 'q', 'Q', '1', 'Ё'],
+    ['KeyW', 'w', 'W', '2', 'Ё'],
+    ['KeyE', 'e', 'E', '3', 'Ё'],
+    ['KeyR', 'r', 'R', '4', 'Ё'],
+    ['KeyT', 't', 'T', '5', 'Ё'],
+    ['KeyY', 'y', 'Y', '6', 'Ё'],
+    ['KeyU', 'u', 'U', '7', 'Ё'],
+    ['KeyI', 'i', 'I', '8', 'Ё'],
+    ['KeyO', 'o', 'O', '9', 'Ё'],
+    ['KeyP', 'p', 'P', '0', 'Ё'],
+    ['BracketLeft', '[', '{', '-', '_'],
+    ['BracketRight', ']', '}', '=', '+'],
+    ['Backslash', '\\', '|', '\\', '/']
 ]
 
 let msg = '';
 
 window.onload = function () {
     let textArea = document.createElement('textarea');
-    document.body.appendChild(textArea);
+    let keyboard = document.createElement('div');
+    document.body.append(textArea);
+    document.body.append(keyboard);
     textArea.className = 'text_area';
-    textArea.focus();
+    keyboard.className = 'keyboard';
+
+    textArea.blur();
+
     let row1 = document.createElement('div');
-    document.body.appendChild(row1);
-    row1.className = 'row1';
-    ROW1.forEach(elem => {
+    let row2 = document.createElement('div');
+    keyboard.append(row1);
+    keyboard.append(row2);
+    createRow('row1', row1, ROW1);
+    createRow('row2', row2, ROW2);
+}
+
+function createRow(rowName, rowWrapper, rowValue) {
+    rowWrapper.className = rowName + ' row';
+    rowValue.forEach(elem => {
         let key = document.createElement('div');
-        row1.append(key);
+        rowWrapper.append(key);
         key.innerText = elem[1];
         key.className = elem[0] + ' key';
     });
@@ -35,36 +63,90 @@ window.onload = function () {
 
 document.addEventListener('keydown', function (event) {
     const textArea = document.querySelector('.text_area');
-    const rows = document.querySelectorAll('.key');
-    rows.forEach((key, index) => {
+    textArea.blur();
+
+    const row1 = document.querySelector('.row1');
+    row1.childNodes.forEach((key, index) => {
         if (event.key === 'Shift') {
             key.innerText = ROW1[index][2];
         }
+    })
+    const row2 = document.querySelector('.row2');
+    row2.childNodes.forEach((key, index) => {
+        if (event.key === 'Shift') {
+            key.innerText = ROW2[index][2];
+        }
+    })
+
+    const rows = document.querySelectorAll('.key');
+    rows.forEach(key => {
         if (key.className.includes(event.code)) {
+            key.style.borderRadius = '16px';
             key.style.background = '#346bd1';
-            msg += key.innerText;
-            textArea.innerText = msg;
+            if (!key.className.includes('Backspace') && !key.className.includes('Tab')) {
+                msg = textArea.value + key.innerText;
+            }
+            if (key.className.includes('Backspace')) {
+                msg = textArea.value.slice(0, -1);
+            }
+            if (key.className.includes('Tab')) {
+                textArea.focus();
+                msg = textArea.value + '\t';
+                textArea.blur();
+            }
+            textArea.value = msg;
         }
     })
 });
 
 document.addEventListener('keyup', function (event) {
-    const rows = document.querySelectorAll('.key');
-    rows.forEach((key, index) => {
+    const row1 = document.querySelector('.row1');
+    row1.childNodes.forEach((key, index) => {
         if (event.key === 'Shift') {
             key.innerText = ROW1[index][1];
         }
+    })
+    const row2 = document.querySelector('.row2');
+    row2.childNodes.forEach((key, index) => {
+        if (event.key === 'Shift') {
+            key.innerText = ROW2[index][1];
+        }
+    })
+
+    const rows = document.querySelectorAll('.key');
+    rows.forEach(key => {
         if (key.className.includes(event.code)) {
+            key.style.borderRadius = '7px';
             key.style.background = '#6094f3';
         }
     })
 });
 
-let form = document.querySelector('body');
-
-form.onclick = function (event) {
+let body = document.querySelector('body');
+body.addEventListener('mousedown', function (event) {
     const textArea = document.querySelector('.text_area');
-    msg += document.querySelector('.' + event.target.className.split(' ')).innerText;
-    textArea.innerText = msg;
-    console.log(document.querySelector('.' + event.target.className.split(' ')));
-};
+    let clickedKeyClass = event.target.className.split(' ');
+
+    if (event.target.className.includes('key')) {
+        if (!clickedKeyClass.includes('Backspace') && !clickedKeyClass.includes('Tab')) {
+            msg = textArea.value + document.querySelector('.' + clickedKeyClass).innerText;
+        }
+        if (clickedKeyClass.includes('Backspace')) {
+            msg = textArea.value.slice(0, -1);
+        }
+        if (clickedKeyClass.includes('Tab')) {
+            msg = textArea.value + '\t';
+        }
+
+        event.target.style.borderRadius = '16px';
+        event.target.style.background = '#346bd1';
+        textArea.value = msg;
+    }
+});
+
+body.addEventListener('mouseup', function (event) {
+    if (event.target.className.includes('key')) {
+        event.target.style.borderRadius = '7px';
+        event.target.style.background = '#6094f3';
+    }
+});
